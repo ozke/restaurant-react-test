2462e09e1ab75fe9cc8897dbede1bfd412c6d77b
Miquel Lopez <m@ozke.com>
2022-12-13 12:35:17 +0100

    Basic setup of the project and first round of work

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import logo from '../../logo.svg';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div class="App">
+    <h1>ravenous</h1>
+    <SearchBar />
+    <BusinessList />
+  </div>
+  );
+}
+
+export default App;
