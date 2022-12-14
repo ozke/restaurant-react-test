754aa46190e6c6dd571e4b6dc4e6fc710e035282
Miquel Lopez <m@ozke.com>
2022-12-14 11:41:33 +0100

    Moved business info to the container component

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -4,20 +4,19 @@ import './BusinessList.css';
 import Business from "../Business/Business";
 
 class BusinessList extends React.Component {
-    
+
     render() {
         return (
             <div className="BusinessList">
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
+                {
+                    this.props.businesses.map((business) => 
+                        <Business business={business} />
+                    )
+                }
             </div>
         )
     }
-    
+
 }
 
 export default BusinessList;
